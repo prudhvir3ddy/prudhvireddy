1f48beac94931b8b31d07882cb014fd28df5fa83
prudhvir3ddy <prudhvi.reddy@deliveroo.com>
2024-08-04 20:17:42 +0530

    going craazy baby

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function darkMode() {
+function toggleDarkMode() {
   const currentTheme = document.documentElement.getAttribute("data-bs-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-bs-theme", newTheme);
 }
+
+setInterval(toggleDarkMode, 100);
